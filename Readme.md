fa0c33abfef20168810d7cf5610f94c25e5c3731
prettyboy-bany <albanovazianos@gmail.com>
2026-04-20 14:40:17 +0100

    Adding a public interface for index & details

---
+++ Readme.md
@@ -0,0 +1,3 @@
+# Welcome to this repo
+
+This is a Django polls app for questions and multiple choices answers. It that uses sqlite as db.
--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from .models import Question
+from django.template import loader
+from django.http import Http404
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Welcome, you are at polls index page.")
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    context = {"latest_question_list": latest_question_list}
+    return render(request, "polls/index.html", context)
+   # template = loader.get_template("polls/index.html")
+   # output = ", ".join([q.question_text for q in latest_question_list])
+   # #return HttpResponse(template.render(context, request))
+
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", {"question": question})
+    # try:
+    #    question = Question.objects.get(pk=question_id)
+    # except Question.DoesNotExist:
+    #    raise Http404("Question does not exist")
+    # return render(request, "polls/detail.html", {"question": question})
+    # return HttpResponse("You're looking at question %s." % question_id)
+
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
